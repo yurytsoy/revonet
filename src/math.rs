d0d935186189f441d1f98ab303f5697b4d7d20d9
yurytsoy <yurytsoy@gmail.com>
2017-05-14 16:27:09 +0900

    Added product of matrix and vector + tests

--- src/math.rs
+++ src/math.rs
@@ -9,8 +9,18 @@ pub fn rand_vector_stdgauss(size: usize, rng: &mut ThreadRng) -> Vec<f32> {
     (0..size).map(|_| normal_rng.ind_sample(rng) as f32).collect::<Vec<f32>>()
 }
 
-pub fn dot_product(x1s: &[f32], x2s: &[f32]) -> f32 {
+#[allow(dead_code)]
+pub fn rand_matrix_stdgauss(height: usize, width: usize, rng: &mut ThreadRng) -> Vec<Vec<f32>> {
+    (0..height).map(|_| rand_vector_stdgauss(width, rng)).collect::<Vec<Vec<f32>>>()
+}
+
+pub fn dot(x1s: &[f32], x2s: &[f32]) -> f32 {
     x1s.iter().zip(x2s).fold(0f32, |s, (&x1, &x2)| s + x1*x2)
+}
+
+pub fn dot_mv(m: &Vec<Vec<f32>>, xs: &[f32]) -> Vec<f32> {
+    // unimplemented!()
+    m.iter().map(|ref row_k| dot(row_k, xs)).collect::<Vec<f32>>()
 }
 
 pub fn min(xs: &Vec<f32>) -> f32 {
@@ -38,22 +48,22 @@ mod tests {
     use math::*;
 
     #[test]
-    fn test_dot_product_zeros() {
+    fn test_dot_zeros() {
         let x1s = [1f32; 100];
         let zeros = [0f32; 100];
-        assert!(dot_product(&x1s, &zeros) == 0f32);
+        assert!(dot(&x1s, &zeros) == 0f32);
     }
 
     #[test]
-    fn test_dot_product_units() {
+    fn test_dot_units() {
         const LEN: usize = 100;
         let x1s = [1f32; LEN];
         let x2s = [1f32; LEN];
-        assert!(dot_product(&x1s, &x2s) == LEN as f32);
+        assert!(dot(&x1s, &x2s) == LEN as f32);
     }
 
     #[test]
-    fn test_dot_product_rand() {
+    fn test_dot_rand() {
         const LEN: usize = 100;
         let mut rng = rand::thread_rng();
         for _ in 0..1000 {
@@ -63,15 +73,43 @@ mod tests {
             for k in 0..LEN {
                 sum += x1s[k] * x2s[k];
             }
-            assert!(dot_product(&x1s, &x2s) == sum);
+            assert!(dot(&x1s, &x2s) == sum);
         }
     }
 
     #[test]
     #[should_panic]
-    fn test_dot_product_unequal_lengths() {
+    fn test_dot_unequal_lengths() {
         let x1s = [1f32; 10];
         let x2s = [1f32; 100];
-        assert!(dot_product(&x1s, &x2s) == 0f32);
+        assert!(dot(&x1s, &x2s) == 0f32);
+    }
+
+    #[test]
+    fn test_dot_mv_zeros() {
+        let x1s = vec![vec![1f32; 10]; 10];
+        let zeros = [0f32; 10];
+        let res = dot_mv(&x1s, &zeros);
+        assert!(res.into_iter().all(|x| x == 0f32));
+    }
+
+    #[test]
+    fn test_dot_mv_rand() {
+        const LEN1: usize = 10;
+        const LEN2: usize = 20;
+        let mut rng = rand::thread_rng();
+        for _ in 0..1000 {
+            let x1s = rand_matrix_stdgauss(LEN2, LEN1, &mut rng);
+            let x2s = rand_vector_stdgauss(LEN1, &mut rng);
+            let dot_res = dot_mv(&x1s, &x2s);
+            assert!(dot_res.len() == LEN2);
+            
+            // println!("{:?}", dot_res);
+            let mut res: Vec<f32> = Vec::with_capacity(LEN2);
+            for k in 0..LEN2 {
+                res.push(dot(&x1s[k], &x2s) - dot_res[k]);
+            }
+            assert!(res.into_iter().all(|x| x == 0f32));
+        }
     }
 }
