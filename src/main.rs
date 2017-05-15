b278df0f7ec3ccf56c163cb155c91728a31f47ad
yurytsoy <yurytsoy@gmail.com>
2017-05-15 23:06:36 +0900

    Added computation of the outputs of NN. Added getting/setting NN weights. Added more tests for NN

--- src/main.rs
+++ src/main.rs
@@ -11,17 +11,6 @@ use ga::*;
 use problem::*;
 use settings::*;
 
-/*
-TODO: Add GA context
-TODO: Make reproducible by enabling seeded rng and passing rng in the context
-TODO: Add saving/loading settings/results to json
-TODO: Add documentation
-TODO: Add support for more crossover and mutation operators
-TODO: Implement multilayered ANN
-TODO: Add test problems for ANN
-TODO: Add speciation
-*/
-
 fn main() {
     let pop_size = 20u32;
     let problem_dim = 10u32;
--- src/neuro.rs
+++ src/neuro.rs
@@ -9,6 +9,7 @@ struct MultilayeredNetwork {
     inputs_num: usize,
     outputs_num: usize,
     layers: Vec<Box<Layer>>,
+    is_built: bool,
 }
 
 #[allow(dead_code)]
@@ -17,7 +18,8 @@ impl MultilayeredNetwork {
         MultilayeredNetwork{
             inputs_num: inputs_num,
             outputs_num: outputs_num,
-            layers: Vec::new()
+            layers: Vec::new(),
+            is_built: false,
         }
     }
 
@@ -25,12 +27,19 @@ impl MultilayeredNetwork {
         self.layers.len()
     }
 
-    pub fn add_layer<T: ActivationFunction + 'static>(&mut self, size: usize) -> &mut Self {
+    pub fn add_hidden_layer<T: ActivationFunction + 'static>(&mut self, size: usize) -> &mut Self {
+        if self.is_built {
+            panic!("Can not add layer to already built network.");
+        }
         self.layers.push(Box::new(NeuralLayer::<T>::new(size)));
         self
     }
 
     pub fn build(&mut self, rng: &mut ThreadRng) {
+        if self.is_built {
+            panic!("The network has already been built.");
+        }
+
         // add output layer.
         self.layers.push(Box::new(NeuralLayer::<LinearActivation>::new(self.outputs_num)));
 
@@ -39,6 +48,42 @@ impl MultilayeredNetwork {
         for l in self.layers.iter_mut() {
             l.init_weights(inputs, rng);
             inputs = l.len();
+        }
+        self.is_built = true;
+    }
+
+    pub fn compute(&mut self, xs: &[f32]) -> Vec<f32> {
+        let mut input = Vec::from(xs);
+        for l in self.layers.iter_mut() {
+            input = l.compute(&input);
+        }
+        Vec::from(input)
+    }
+
+    /// Returns weights and biases layer by layer.
+    pub fn get_weights(&self) -> (Vec<Vec<f32>>, Vec<Vec<f32>>) {
+        if !self.is_built {
+            panic!("Can not retreive network weights: the network is not built yet.");
+        }
+
+        let mut res_w = Vec::new();
+        let mut res_b = Vec::new();
+        for l in self.layers.iter() {
+            let (cur_w, cur_b) = l.get_weights();
+            res_w.push(cur_w);
+            res_b.push(cur_b);
+        }
+        (res_w, res_b)
+    }
+
+    /// Sets weights and biases layer by layer.
+    pub fn set_weights(&mut self, wss: &Vec<Vec<f32>>, bss: &Vec<Vec<f32>>) {
+        if !self.is_built {
+            panic!("Can not set network weights: the network is not built yet.");
+        }
+
+        for k in 0..self.layers.len() {
+            self.layers[k].set_weights(&wss[k], &bss[k]);
         }
     }
 }
@@ -49,6 +94,8 @@ trait Layer: Debug {
     fn init_weights(&mut self, inputs_num: usize, rng: &mut ThreadRng);
     fn compute(&mut self, xs: &[f32]) -> Vec<f32>;
     fn len(&self) -> usize;
+    fn get_weights(&self) -> (Vec<f32>, Vec<f32>);
+    fn set_weights(&mut self, ws: &Vec<f32>, bs: &Vec<f32>);
 }
 
 #[derive(Debug)]
@@ -58,7 +105,7 @@ struct NeuralLayer<T: ActivationFunction> {
     weights: Vec<Vec<f32>>,
     biases: Vec<f32>,
     outputs: Vec<f32>,
-    activation: Vec<T>,
+    activations: Vec<T>,
 }
 
 #[allow(dead_code)]
@@ -69,7 +116,7 @@ impl<T: ActivationFunction> NeuralLayer<T> {
             weights: Vec::new(),
             biases: Vec::new(),
             outputs: Vec::new(),
-            activation: (0..size).map(|_| T::new()).collect::<Vec<T>>()
+            activations: (0..size).map(|_| T::new()).collect::<Vec<T>>()
         }
     }
 }
@@ -77,21 +124,41 @@ impl<T: ActivationFunction> NeuralLayer<T> {
 #[allow(dead_code)]
 impl<T: ActivationFunction> Layer for NeuralLayer<T> {
     fn init_weights(&mut self, inputs_num: usize, rng: &mut ThreadRng) {
+        // println!("Init weights: {} nodes, {} inputs", self.size, inputs_num);
         for _ in 0..self.size {
             self.weights.push(rand_vector_std_gauss(inputs_num, rng));
         }
-        self.biases = rand_vector_std_gauss(inputs_num, rng);
+        self.biases = rand_vector_std_gauss(self.size, rng);
     }
     
     fn compute(&mut self, xs: &[f32]) -> Vec<f32> {
         self.outputs = dot_mv(&self.weights, &xs);
         self.outputs = (0..self.outputs.len())
-                            .map(|k| self.activation[k].compute(self.outputs[k] + self.biases[k]))
+                            .map(|k| self.activations[k].compute(self.outputs[k] + self.biases[k]))
                             .collect::<Vec<f32>>();
         self.outputs.clone()
     }
 
     fn len(&self) -> usize {self.size}
+
+    fn get_weights(&self) -> (Vec<f32>, Vec<f32>) {
+        let mut res_w = Vec::new();
+        let mut res_b = Vec::with_capacity(self.size);
+        
+        for k in 0..self.size {
+            res_w.extend(self.weights[k].clone());
+            res_b.push(self.biases[k]);
+        }
+        (res_w, res_b)
+    }
+
+    fn set_weights(&mut self, ws: &Vec<f32>, bs: &Vec<f32>) {
+        let inputs = self.weights[0].len();
+        for k in 0..self.size {
+            self.weights[k] = Vec::from(&ws[k*inputs..(k+1)*inputs]);
+            self.biases[k] = bs[k];
+        }
+    }
 }
 
 
@@ -148,28 +215,55 @@ mod test {
     }
 
     #[test]
-    fn test_linear_multilayer_net() {
+    fn test_linear_net() {
         const INPUT_SIZE: usize = 20;
         const OUTPUT_SIZE: usize = 2;
 
         let mut rng = rand::thread_rng();
-        let mut net = MultilayeredNetwork::new(INPUT_SIZE, OUTPUT_SIZE);
-        net.add_layer::<SigmoidActivation>(30 as usize)
-            .add_layer::<LinearActivation>(15 as usize)
-            .add_layer::<SigmoidActivation>(300 as usize)
-            .build(&mut rng);
-        
+        let mut net_linear: MultilayeredNetwork = MultilayeredNetwork::new(INPUT_SIZE, OUTPUT_SIZE);
+        net_linear.add_hidden_layer::<LinearActivation>(30 as usize).build(&mut rng);
 
-        // println!("{:?}", net);
-        // for k in 0..net.len() {
-        //     println!("{}: {:?}", k, net.layers[k])
-        // }
+        let mut zero_w = Vec::new();
+        zero_w.push(vec![0f32; INPUT_SIZE * 30]);
+        zero_w.push(vec![0f32; OUTPUT_SIZE * 30]);
+        let mut zero_b = Vec::new();
+        zero_b.push(vec![0f32; 30]);
+        zero_b.push(vec![0f32; OUTPUT_SIZE]);
+        net_linear.set_weights(&zero_w, &zero_b);
+        let net_out = net_linear.compute(&[0f32; INPUT_SIZE]);
+        assert!(net_out.iter().all(|&x| x == 0f32));
 
-        // let mut rng = rand::thread_rng();
-        // let Neurallayer = NeuralLayer::new::<LinearActivation>(NeuralLAYER_SIZE);
-        // let xs = rand_vector_stdgauss(LEN, &mut rng);
-        // let act_f = LinearActivation{};
-        // assert!((0..LEN).all(|k| xs[k] == act_f.compute(xs[k])));
+        let net_out = net_linear.compute(&rand_vector_std_gauss(INPUT_SIZE, &mut rng));
+        assert!(net_out.iter().all(|&x| x == 0f32));
     }
 
+    #[test]
+    fn test_multilayer_net() {
+        const INPUT_SIZE: usize = 20;
+        const OUTPUT_SIZE: usize = 2;
+
+        let mut rng = rand::thread_rng();
+        let mut net: MultilayeredNetwork = MultilayeredNetwork::new(INPUT_SIZE, OUTPUT_SIZE);
+        net.add_hidden_layer::<SigmoidActivation>(30 as usize)
+            .add_hidden_layer::<LinearActivation>(15 as usize)
+            .add_hidden_layer::<SigmoidActivation>(300 as usize)
+            .build(&mut rng);
+
+        // net2 has the same structure and weights as net1.
+        let mut net2: MultilayeredNetwork = MultilayeredNetwork::new(INPUT_SIZE, OUTPUT_SIZE);
+        net2.add_hidden_layer::<SigmoidActivation>(30 as usize)
+            .add_hidden_layer::<LinearActivation>(15 as usize)
+            .add_hidden_layer::<SigmoidActivation>(300 as usize)
+            .build(&mut rng);
+        let (ws, bs) = net.get_weights();
+        net2.set_weights(&ws, &bs);
+
+        // net and net2 should produce identical outputs
+        for _ in 0..100 {
+            let x = rand_vector_std_gauss(INPUT_SIZE, &mut rng);
+            let out1 = net.compute(&x);
+            let out2 = net2.compute(&x);
+            assert!(out1.iter().zip(out2.iter()).all(|(x1, x2)| x1 == x2));
+        }
+    }
 }
